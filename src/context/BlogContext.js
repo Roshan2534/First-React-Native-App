8b144d23c3d91b8edad197780e768ef547e2f408
Roshan2534 <roshansaundankar67@gmail.com>
2020-03-11 23:35:53 -0700

    Added ngrok

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,10 @@
 import createDataContext from './createDataContext';
+import jsonServer from '../api/jsonServer';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
+        case 'get_blogposts':
+            return action.payload;
         case 'edit_blogpost':
             return state.map((blogPost) => {
                 return blogPost.id === action.payload.id
@@ -21,13 +24,21 @@ const blogReducer = (state, action) => {
     }
 };
 
-const addBlogPost = dispatch => {
-    return (title, content, callback) => {
-        dispatch({type: 'add_blogpost', payload: {title, content}});
-        if (callback) {
-            callback();
-        }
+const getBlogPosts = dispatch => {
+    return async () => {
+        const response = await jsonServer.get('/blogposts');
+        dispatch({type:'get_blogposts', payload: response.data});
     };
+};
+
+const addBlogPost = dispatch => {
+        return async (title, content, callback) => {
+            await jsonServer.post('/blogposts', title, content)
+    //     dispatch({type: 'add_blogpost', payload: {title, content}});
+    //     if (callback) {
+    //         callback();
+    //     }
+     };
    };
 
 const deleteBlogPost = dispatch => {
@@ -46,6 +57,6 @@ const editBlogPost = dispatch => {
 };
 
 export const  { Context, Provider } = createDataContext(blogReducer, 
-    {addBlogPost, deleteBlogPost, editBlogPost},
-    [{title: 'Test Post', content: 'Test Content', id: 1}]
+    {addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts},
+    []
     );
